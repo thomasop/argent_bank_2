1a0ef17fff2e50ebf471275eba035a1588463061
thomasop <tdss33@hotmail.com>
2023-05-15 10:43:41 +0200

    Add: static Html and CSS for all Pages

---
+++ src/Pages/Login.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+
+const Login: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <main className="main bg-dark">
+        <section className="sign-in-content">
+          <i className="fa fa-user-circle sign-in-icon"></i>
+          <h1>Sign In</h1>
+          <form>
+            <div className="input-wrapper">
+              <label htmlFor="username">Username</label>
+              <input type="text" id="username" />
+            </div>
+            <div className="input-wrapper">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" />
+            </div>
+            <div className="input-remember">
+              <input type="checkbox" id="remember-me" />
+              <label htmlFor="remember-me">Remember me</label>
+            </div>
+            <button className="sign-in-button">Sign In</button>
+          </form>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Login;
