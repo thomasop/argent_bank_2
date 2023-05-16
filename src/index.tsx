ecc7c4f41eb105213cdd56b829a19394200a8e75
thomasop <tdss33@hotmail.com>
2023-05-16 12:11:13 +0200

    Add: router with routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import "./css/style.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./css/style.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Profil from "./Pages/Profil";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Profil" element={<Profil />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
